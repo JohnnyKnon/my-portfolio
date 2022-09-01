b2b507ae43a5d9565e08eb9455e01cc5fbbff877
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-09-01 13:52:44 +0900

    reset / basic setting

---
+++ vue-project/src/views/Main.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+  import HelloWorldVue from '../components/HelloWorld.vue';
+</script>
+
+
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <main>
+   <HelloWorldVue/>
+  </main>
+</template>
+
+<style></style>
